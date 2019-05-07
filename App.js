eaa7e50cf36d6f27bbfe29b1740ae411a27716d0
hieumdang <45186193+hieumdang@users.noreply.github.com>
2019-05-07 00:28:15 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react'
+import './App.css'
+import Title from '.components/NameCalling
+
+
+class App extends Component {
+  render()  {
+    return (
+    <div className = 'App>                  //class is a reverse word, cant use
+      <Tittle name = "Jon Snow" otherName= "King of 7 Kingdoms">
+      <Tittle name = "Robb Stark" otherName ="King in The North">
+      <Tittle name = "Ned Stark" otherName = "Warden of The North">
+     </div>
+     )
+  }
+}
+     
