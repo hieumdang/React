f3b88d7887c8d49ac83368eb5ce787f4bc9a76b2
hieumdang <45186193+hieumdang@users.noreply.github.com>
2019-05-07 00:34:03 -0700

    Update App.js

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ class App extends Component {
   render()  {
     return (
     <div className = 'App>                  //class is a reverse word, cant use
-      <Tittle name = "Jon Snow" otherName= "King of 7 Kingdoms">
+      <Tittle name = "Jon Snow"   otherName= "King of 7 Kingdoms">
       <Tittle name = "Robb Stark" otherName ="King in The North">
-      <Tittle name = "Ned Stark" otherName = "Warden of The North">
+      <Tittle name = "Ned Stark"  otherName = "Warden of The North">
      </div>
      )
   }
